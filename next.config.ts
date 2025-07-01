fc871c093073278d82418d53393cdc5003c07324
yomjsty <oweychanges@gmail.com>
2025-07-01 09:30:33 +0700

    prisma workaround

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextConfig } from "next";
+//@ts-ignore
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,7 +12,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
       }
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
