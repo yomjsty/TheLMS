93ad8f27b17eaea3bb8eb92d1e1c79a2c7aff1b9
yomjsty <oweychanges@gmail.com>
2025-07-01 09:48:49 +0700

    arcjet bot denied the stripe purchase (webhook)

--- middleware.ts
+++ middleware.ts
@@ -37,7 +37,7 @@ async function authMiddleware(request: NextRequest) {
 export const config = {
     // matcher tells Next.js which routes to run the middleware on.
     // This runs the middleware on all routes except for static assets.
-    matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth|api/webhook/stripe).*)"],
 };
 
 // Pass any existing middleware with the optional existingMiddleware prop
